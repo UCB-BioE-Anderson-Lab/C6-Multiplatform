864aa4292a2b57b09bf2f38cbc92c45f31dc0053
Richiewoohoo <richie.woo@berkeley.edu>
2025-08-17 02:21:36 -0700

    First edition of input verification framework

---
+++ js-gs-automation/Example-Automation.js
@@ -0,0 +1,21 @@
+function JS_someFunc(someSeq, someString, someBoolean){
+    //someSeq may be Polynucleotide or String
+    //someString is one of four that will be checked in-function
+    //someBoolean is a bool used to turn certain functions on and off.
+    return "arbitrary output"
+}
+
+function someFunc(...inputArray){
+    // Initialize predefined variables
+    varDict = {
+        "someSeq":["String", "Polynucleotide"],
+        "someString":["String"],
+        "someBoolean":["Bool"],
+    };
+
+    // Verify inputs using the given dictionary of acceptable inputs.
+    // Then, call the base function with those inputs.
+    // Last, verify the outputs so objects are returned as string forms.
+    return verifyOutputs(JS_someFunc(verifyInputs(varDict, inputArray)));
+}
+
