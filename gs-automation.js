7299235bf4702cabd41829e7827fdea41b9871ce
Richiewoohoo <richie.woo@berkeley.edu>
2025-08-19 01:59:20 -0700

    Added and modified outputs, added execution time to automation.

--- gs-automation.js
+++ gs-automation.js
@@ -7,13 +7,17 @@
 
 import * as fs from "fs";
 
+console.time("Execution Time");
+
+console.log("Beginning JS Module to Apps Script conversion.");
+
 var rawFileData = new String();
 
-// Read rolled up IIFE module file into memory.
-// IIFE module file was created in the previous command in the "npm run buildgs" chain
+// Read bundled UMD module file into memory.
+// UMD module file was created by running "npm run build"
 try {
   const data = fs.readFileSync('dist/c6-sim.min.js', 'utf8');
-  rawFileData = data.toString()
+  rawFileData = data.toString();
 } catch (err) {
   console.error('Error reading file synchronously:', err);
 }
@@ -26,14 +30,19 @@ const regexesToMatch = [
   /^  /gm // Untabs entire file
 ]
 
-// Rollup does not allow you to create a "plain" file, so the rolled up file needs to have its modulization reversed.
+// Rollup does not allow you to create a "plain" file, so the bundled file needs to be freed from its 
+// factory function and any other modulizations.
 for (const regex of regexesToMatch) {
   rawFileData = rawFileData.replace(regex, "");
 }
 
-// Prepend all function names with "JS_" to indicate this function is unadulterated from the module file.
+// Prepend all function names with "JS_" to indicate this function is the original from the module file.
 const funcRegex = /^function /gm;
-rawFileData = rawFileData.replace(funcRegex, "function JS_")
+rawFileData = rawFileData.replace(funcRegex, "function JS_");
 
 // Write finished file to local storage.
-fs.writeFileSync('js-gs-automation/sheets.js', rawFileData);
+fs.writeFileSync('js-gs-automation/C6-Multiplatform-Raw.js', rawFileData);
+fs.writeFileSync('dist_appsscript/C6-Multiplatform-Raw.gs', rawFileData);
+
+console.timeEnd("Execution Time");
+console.log("Finished JS Module to Apps Script conversion.");
