ec8ffe4b627fbbcf6d580125a9197653772ae022
Richiewoohoo <richie.woo@berkeley.edu>
2025-08-21 00:47:16 -0700

    Untab large chunk of C6-Seq to prevent automation errors

--- src/C6-Seq.js
+++ src/C6-Seq.js
@@ -237,258 +237,258 @@ function polyrevcomp(frag) {
   );
 }
 
-  /**
-  Creates a new polynucleotide (DNA or RNA) object as JSON
-  @param {string} sequence - The sequence of the polynucleotide.
-  @param {string} ext5 - The 5' extension of the current coding strand.
-  @param {string} ext3 - The 3' extension of the current coding strand.
-  @param {boolean} isDoubleStranded - Whether the polynucleotide is double stranded.
-  @param {boolean} isRNA - Whether the polynucleotide is RNA.
-  @param {boolean} isCircular - Whether the polynucleotide is circular.
-  @param {string} mod_ext5 - The 5' end modification.
-  @param {string} mod_ext3 - The 3' end modification.
-  @return {String} The created polynucleotide object as JSON.
-  @example
-  var polynucleotide = polynucleotide("AGCTAGCT", "GATC", "CTAG", true, false, false, null, null);
-  @customfunction
-  */
+/**
+Creates a new polynucleotide (DNA or RNA) object as JSON
+@param {string} sequence - The sequence of the polynucleotide.
+@param {string} ext5 - The 5' extension of the current coding strand.
+@param {string} ext3 - The 3' extension of the current coding strand.
+@param {boolean} isDoubleStranded - Whether the polynucleotide is double stranded.
+@param {boolean} isRNA - Whether the polynucleotide is RNA.
+@param {boolean} isCircular - Whether the polynucleotide is circular.
+@param {string} mod_ext5 - The 5' end modification.
+@param {string} mod_ext3 - The 3' end modification.
+@return {String} The created polynucleotide object as JSON.
+@example
+var polynucleotide = polynucleotide("AGCTAGCT", "GATC", "CTAG", true, false, false, null, null);
+@customfunction
+*/
 
-  function polynucleotide(sequence, ext5, ext3, isDoubleStranded, isRNA, isCircular, mod_ext5, mod_ext3) {
-    var out = new Polynucleotide(sequence, ext5, ext3, isDoubleStranded, isRNA, isCircular, mod_ext5, mod_ext3);;
-    return out;
+function polynucleotide(sequence, ext5, ext3, isDoubleStranded, isRNA, isCircular, mod_ext5, mod_ext3) {
+  var out = new Polynucleotide(sequence, ext5, ext3, isDoubleStranded, isRNA, isCircular, mod_ext5, mod_ext3);;
+  return out;
+}
+
+/**
+* Creates a Polynucleotide object (as JSON) representing a blunt-ended, double-stranded DNA
+* such as results from PCR or GBlock synthesis.  It lacks 5' phosphates.
+* @param {string} sequence - The coding strand sequence of the dsDNA.
+* @return {string} The string representation of the created polynucleotide as JSON.
+* @example
+* var frag = dsDNA("AGCTAGCT");
+* console.log(frag); // Output: '{"sequence":"AGCTAGCT","ext5":null,"ext3":null,"isDoubleStranded":true,
+* "isRNA":false,"isCircular":false,"mod_ext5":null,"mod_ext3":null}'
+* @customfunction
+*/
+
+function dsDNA(sequence) {
+  return new Polynucleotide(sequence, "", "", true, false, false, "hydroxyl", "hydroxyl");
+}
+
+/**
+* Creates a Polynucleotide object (as JSON) representing a linear single stranded DNA (an oligonucleotide)
+* @param {string} sequence - The sequence of the polynucleotide oligo.
+* @return {string} The string representation of the created polynucleotide oligo.
+* @example
+* var oligo = oligo("AGCTAGCT");
+* console.log(oligo); // Output: '{"sequence":"AGCTAGCT","ext5":null,"ext3":null,"isDoubleStranded":false
+* "isRNA":false,"isCircular":false,"mod_ext5":null,"
+*/
+
+function oligo(sequence) {
+  return new Polynucleotide(sequence, null, null, false, false, false, "hydroxyl", null);
+}
+
+/**
+* Creates a Polynucleotide object (as JSON) representing a circular doubkle stranded DNA (a plasmid)
+* @param {string} sequence - The sequence of the polynucleotide plasmid.
+* @return {string} The string representation of the created polynucleotide plasmid.
+* @example
+* var plasmid = plasmid("AGCTAGCT");
+console.log(plasmid); // Output: '{"sequence":"AGCTAGCT","ext5":null,"ext3":null,"isDoubleStranded":true,"isRNA":false,"isCircular":true,"mod_ext5":null,"mod_ext3":null}'
+*/
+
+function plasmid(sequence) {
+  return new Polynucleotide(sequence, "", "", true, false, true, null, null);
+}
+
+/**
+ * Not to be called from Sheets
+ * For resolving a string to a Polynucleotide object
+ */
+
+function resolveToPoly(seqOrJSON) {
+  //See if its a JSON already
+  try{
+      var json = JSON.parse(seqOrJSON);
+      return json;
+  }
+  catch(err) {/*intentionally empty*/}
+
+  //See if its a singular DNA sequence
+  if (!_regexDNA.test(seqOrJSON)) {
+    throw Error("Cannot resolve " + seqOrJSON);
   }
   
-  /**
-  * Creates a Polynucleotide object (as JSON) representing a blunt-ended, double-stranded DNA
-  * such as results from PCR or GBlock synthesis.  It lacks 5' phosphates.
-  * @param {string} sequence - The coding strand sequence of the dsDNA.
-  * @return {string} The string representation of the created polynucleotide as JSON.
-  * @example
-  * var frag = dsDNA("AGCTAGCT");
-  * console.log(frag); // Output: '{"sequence":"AGCTAGCT","ext5":null,"ext3":null,"isDoubleStranded":true,
-  * "isRNA":false,"isCircular":false,"mod_ext5":null,"mod_ext3":null}'
-  * @customfunction
-  */
+  return new Polynucleotide(seqOrJSON, "", "", true, false, false, "hydroxyl", "hydroxyl");
+}
 
-  function dsDNA(sequence) {
-    return new Polynucleotide(sequence, "", "", true, false, false, "hydroxyl", "hydroxyl");
-  }
-  
-  /**
-  * Creates a Polynucleotide object (as JSON) representing a linear single stranded DNA (an oligonucleotide)
-  * @param {string} sequence - The sequence of the polynucleotide oligo.
-  * @return {string} The string representation of the created polynucleotide oligo.
-  * @example
-  * var oligo = oligo("AGCTAGCT");
-  * console.log(oligo); // Output: '{"sequence":"AGCTAGCT","ext5":null,"ext3":null,"isDoubleStranded":false
-  * "isRNA":false,"isCircular":false,"mod_ext5":null,"
-  */
+/**
+ * Determines whether a given DNA sequence is palindromic.
+ * A palindromic DNA sequence is one that reads the same forward and backward when complemented.
+ * This function also checks for invalid characters in the input sequence and throws an exception if any are found.
+ *
+ * @param {string} seq - The input DNA sequence to be checked for palindromicity.
+ * @return {boolean} - Returns true if the input DNA sequence is palindromic, false otherwise.
+ * @throws {Error} - Throws an error if the input sequence contains characters other than A, T, C, or G.
+ *
+ * Usage:
+ *   const result = isPalindromic("AATT");
+ *   console.log(result); // Output: true
+ *
+ * Example:
+ *   1. isPalindromic("AATT") returns true
+ *   2. isPalindromic("AGCT") returns false
+ *   3. isPalindromic("GAATTC") returns true
+ *   4. isPalindromic("AATN") throws an error (invalid character 'N')
+ */
 
-  function oligo(sequence) {
-    return new Polynucleotide(sequence, null, null, false, false, false, "hydroxyl", null);
-  }
-  
-  /**
-  * Creates a Polynucleotide object (as JSON) representing a circular doubkle stranded DNA (a plasmid)
-  * @param {string} sequence - The sequence of the polynucleotide plasmid.
-  * @return {string} The string representation of the created polynucleotide plasmid.
-  * @example
-  * var plasmid = plasmid("AGCTAGCT");
-  console.log(plasmid); // Output: '{"sequence":"AGCTAGCT","ext5":null,"ext3":null,"isDoubleStranded":true,"isRNA":false,"isCircular":true,"mod_ext5":null,"mod_ext3":null}'
-  */
+function isPalindromic(seq) {
+  const complements = {
+    'A': 'T',
+    'T': 'A',
+    'C': 'G',
+    'G': 'C',
+  };
 
-  function plasmid(sequence) {
-    return new Polynucleotide(sequence, "", "", true, false, true, null, null);
-  }
-  
-  /**
-   * Not to be called from Sheets
-   * For resolving a string to a Polynucleotide object
-   */
-
-  function resolveToPoly(seqOrJSON) {
-    //See if its a JSON already
-    try{
-        var json = JSON.parse(seqOrJSON);
-        return json;
+  // Check for invalid characters and throw an exception if found
+  for (const nucleotide of seq) {
+    if (!complements.hasOwnProperty(nucleotide)) {
+      throw new Error(`Error: Invalid character '${nucleotide}' found in sequence '${seq}'. Sequence must contain only A, T, C, or G.`);
     }
-    catch(err) {/*intentionally empty*/}
-  
-    //See if its a singular DNA sequence
-    if (!_regexDNA.test(seqOrJSON)) {
-      throw Error("Cannot resolve " + seqOrJSON);
-    }
-    
-    return new Polynucleotide(seqOrJSON, "", "", true, false, false, "hydroxyl", "hydroxyl");
   }
-  
-  /**
-   * Determines whether a given DNA sequence is palindromic.
-   * A palindromic DNA sequence is one that reads the same forward and backward when complemented.
-   * This function also checks for invalid characters in the input sequence and throws an exception if any are found.
-   *
-   * @param {string} seq - The input DNA sequence to be checked for palindromicity.
-   * @return {boolean} - Returns true if the input DNA sequence is palindromic, false otherwise.
-   * @throws {Error} - Throws an error if the input sequence contains characters other than A, T, C, or G.
-   *
-   * Usage:
-   *   const result = isPalindromic("AATT");
-   *   console.log(result); // Output: true
-   *
-   * Example:
-   *   1. isPalindromic("AATT") returns true
-   *   2. isPalindromic("AGCT") returns false
-   *   3. isPalindromic("GAATTC") returns true
-   *   4. isPalindromic("AATN") throws an error (invalid character 'N')
-   */
 
-  function isPalindromic(seq) {
-    const complements = {
-      'A': 'T',
-      'T': 'A',
-      'C': 'G',
-      'G': 'C',
-    };
-  
-    // Check for invalid characters and throw an exception if found
-    for (const nucleotide of seq) {
-      if (!complements.hasOwnProperty(nucleotide)) {
-        throw new Error(`Error: Invalid character '${nucleotide}' found in sequence '${seq}'. Sequence must contain only A, T, C, or G.`);
-      }
-    }
-  
-    const reverseComplement = seq.split('').reverse().map(nucleotide => complements[nucleotide]).join('');
-    return seq === reverseComplement;
+  const reverseComplement = seq.split('').reverse().map(nucleotide => complements[nucleotide]).join('');
+  return seq === reverseComplement;
+}
+
+
+/**
+ * Calculates the reverse complement of a DNA sequence, including handling of degeneracy codes.
+ *
+ * @param {string} inseq - The DNA sequence to reverse complement
+ * @return {string} The reverse complement of the DNA sequence, or "N/A" if the input contains invalid characters
+ */
+
+function revcomp(inseq) {
+  if(!inseq.length) {
+    return "error on " + inseq;
   }
-  
-  
-  /**
-   * Calculates the reverse complement of a DNA sequence, including handling of degeneracy codes.
-   *
-   * @param {string} inseq - The DNA sequence to reverse complement
-   * @return {string} The reverse complement of the DNA sequence, or "N/A" if the input contains invalid characters
-   */
 
-  function revcomp(inseq) {
-    if(!inseq.length) {
-      return "error on " + inseq;
+  var output = "";
+  for (let i = inseq.length-1; i >= 0; i--) {
+    switch(inseq[i]) {
+      case 'A': { output += 'T'; continue; }
+      case 'T': { output += 'A'; continue; }
+      case 'C': { output += 'G'; continue; }
+      case 'G': { output += 'C'; continue; }
+      case 'a': { output += 't'; continue; }
+      case 't': { output += 'a'; continue; }
+      case 'c': { output += 'g'; continue; }
+      case 'g': { output += 'c'; continue; }
+
+      case 'B': { output += 'V'; continue; }
+      case 'D': { output += 'H'; continue; }
+      case 'H': { output += 'D'; continue; }
+      case 'K': { output += 'M'; continue; }
+      case 'N': { output += 'N'; continue; }
+      case 'R': { output += 'Y'; continue; }
+      case 'S': { output += 'S'; continue; }
+      case 'M': { output += 'K'; continue; }
+      case 'V': { output += 'B'; continue; }
+      case 'W': { output += 'W'; continue; }
+      case 'Y': { output += 'R'; continue; }
+
+      case 'b': { output += 'v'; continue; }
+      case 'd': { output += 'h'; continue; }
+      case 'h': { output += 'd'; continue; }
+      case 'k': { output += 'm'; continue; }
+      case 'n': { output += 'n'; continue; }
+      case 'r': { output += 'y'; continue; }
+      case 's': { output += 's'; continue; }
+      case 'm': { output += 'k'; continue; }
+      case 'v': { output += 'b'; continue; }
+      case 'w': { output += 'w'; continue; }
+      case 'y': { output += 'r'; continue; }
+      
+      default:  throw new Error("Character '" + inseq[i] + "' is not a valid DNA character");
     }
-  
-    var output = "";
-    for (let i = inseq.length-1; i >= 0; i--) {
-      switch(inseq[i]) {
-        case 'A': { output += 'T'; continue; }
-        case 'T': { output += 'A'; continue; }
-        case 'C': { output += 'G'; continue; }
-        case 'G': { output += 'C'; continue; }
-        case 'a': { output += 't'; continue; }
-        case 't': { output += 'a'; continue; }
-        case 'c': { output += 'g'; continue; }
-        case 'g': { output += 'c'; continue; }
-  
-        case 'B': { output += 'V'; continue; }
-        case 'D': { output += 'H'; continue; }
-        case 'H': { output += 'D'; continue; }
-        case 'K': { output += 'M'; continue; }
-        case 'N': { output += 'N'; continue; }
-        case 'R': { output += 'Y'; continue; }
-        case 'S': { output += 'S'; continue; }
-        case 'M': { output += 'K'; continue; }
-        case 'V': { output += 'B'; continue; }
-        case 'W': { output += 'W'; continue; }
-        case 'Y': { output += 'R'; continue; }
-  
-        case 'b': { output += 'v'; continue; }
-        case 'd': { output += 'h'; continue; }
-        case 'h': { output += 'd'; continue; }
-        case 'k': { output += 'm'; continue; }
-        case 'n': { output += 'n'; continue; }
-        case 'r': { output += 'y'; continue; }
-        case 's': { output += 's'; continue; }
-        case 'm': { output += 'k'; continue; }
-        case 'v': { output += 'b'; continue; }
-        case 'w': { output += 'w'; continue; }
-        case 'y': { output += 'r'; continue; }
-        
-        default:  throw new Error("Character '" + inseq[i] + "' is not a valid DNA character");
-      }
-    }
-    return output;
   }
-           
-  
-  /**
-   * Calculates the G/C content of a DNA sequence.
-   *
-   * @param {string} inseq - The DNA sequence to analyze
-   * @return {number} The G/C content of the DNA sequence, between 0 and 1
-   */
+  return output;
+}
+          
 
-  function gccontent(inseq) {
+/**
+ * Calculates the G/C content of a DNA sequence.
+ *
+ * @param {string} inseq - The DNA sequence to analyze
+ * @return {number} The G/C content of the DNA sequence, between 0 and 1
+ */
+
+function gccontent(inseq) {
+  inseq = inseq.toUpperCase();
+  let gcCount = 0;
+  for (let i = 0; i < inseq.length; i++) {
+    if (inseq[i] == "G" || inseq[i] == "C") {
+      gcCount++;
+    }
+  }
+  return gcCount / inseq.length;
+}
+
+/**
+ * Calculates the base balance of a DNA sequence.
+ *
+ * @param {string} inseq - The DNA sequence to analyze
+ * @return {number} The base balance of the DNA sequence, between 0 and 1
+ */
+
+function basebalance(inseq) {
     inseq = inseq.toUpperCase();
-    let gcCount = 0;
-    for (let i = 0; i < inseq.length; i++) {
-      if (inseq[i] == "G" || inseq[i] == "C") {
-        gcCount++;
-      }
-    }
-    return gcCount / inseq.length;
+  let baseCounts = {
+    A: 0,
+    C: 0,
+    G: 0,
+    T: 0,
+  };
+  for (let i = 0; i < inseq.length; i++) {
+    baseCounts[inseq[i]]++;
   }
-  
-  /**
-   * Calculates the base balance of a DNA sequence.
-   *
-   * @param {string} inseq - The DNA sequence to analyze
-   * @return {number} The base balance of the DNA sequence, between 0 and 1
-   */
+  let score = 1;
+  for (const base in baseCounts) {
+    if (baseCounts[base] == 0) {
+      score = 0;
+      break;
+    }
+    score *= baseCounts[base] / inseq.length;
+  }
+  return 4*Math.pow(score, 1/4);
+}
 
-  function basebalance(inseq) {
-      inseq = inseq.toUpperCase();
-    let baseCounts = {
-      A: 0,
-      C: 0,
-      G: 0,
-      T: 0,
-    };
-    for (let i = 0; i < inseq.length; i++) {
-      baseCounts[inseq[i]]++;
-    }
-    let score = 1;
-    for (const base in baseCounts) {
-      if (baseCounts[base] == 0) {
-        score = 0;
-        break;
-      }
-      score *= baseCounts[base] / inseq.length;
-    }
-    return 4*Math.pow(score, 1/4);
-  }
-  
-  /**
-   * Finds the longest streak of repeating bases in a DNA sequence.
-   *
-   * @param {string} inseq - The DNA sequence to analyze
-   * @return {number} The longest streak of repeating bases in the DNA sequence
-   */
+/**
+ * Finds the longest streak of repeating bases in a DNA sequence.
+ *
+ * @param {string} inseq - The DNA sequence to analyze
+ * @return {number} The longest streak of repeating bases in the DNA sequence
+ */
 
-  function maxrepeat(inseq) {
-    inseq = inseq.toUpperCase();
-  
-    let lastBase = "";
-    let streak = 0;
-    let maxStreak = 0;
-    for (let i = 0; i < inseq.length; i++) {
-      if (inseq[i] == lastBase) {
-        streak++;
-        maxStreak = Math.max(maxStreak, streak);
-      } else {
-        lastBase = inseq[i];
-        streak = 1;
-      }
+function maxrepeat(inseq) {
+  inseq = inseq.toUpperCase();
+
+  let lastBase = "";
+  let streak = 0;
+  let maxStreak = 0;
+  for (let i = 0; i < inseq.length; i++) {
+    if (inseq[i] == lastBase) {
+      streak++;
+      maxStreak = Math.max(maxStreak, streak);
+    } else {
+      lastBase = inseq[i];
+      streak = 1;
     }
-    return maxStreak;
   }
-  
+  return maxStreak;
+}
+
 
 /**
  * Translates a DNA sequence to an amino acid sequence (single-letter codes, no stop codons).
